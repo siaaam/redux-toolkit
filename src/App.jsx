e25e6c0c1afe4ddf7f1fbf1e675a04abede5b089
siaaam <abdullahsiam009@gmail.com>
2023-02-24 02:53:17 +0600

    redux toolkit setup and create a simple counter app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Counter from "./app/features/counter/Counter";
+
+function App() {
+  return (
+    <main className="App">
+      <Counter />
+    </main>
+  );
+}
+
+export default App;
